6c528a3d0c8b97e7b1ce521db03e60407330e59d
Mauricio <mauriciofigueiredo@knights.ucf.edu>
2020-06-10 15:10:02 -0400

    Fixed issue with config.js file

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    consumer_key:         process.env.DATABASE_URL.consumer_key,
+    consumer_key:         process.env.consumer_key,
     consumer_secret:      process.env.consumer_secret,
     access_token:         process.env.access_token,
     access_token_secret:  process.env.access_token_secret
